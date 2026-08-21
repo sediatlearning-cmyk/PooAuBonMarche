f8359777dd6dc8e7fd6e5794b47ccf3fa1cda93a
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-08-21 13:27:37 +0200

    Added abstract Product class

--- src/fr/fms/entities/Product.java
+++ src/fr/fms/entities/Product.java
@@ -1,10 +1,49 @@
 package fr.fms.entities;
-
-public class Product {
+/*
+ * represents a new product
+ */
+public abstract class Product {
 	
 	private String name;
 	private int stock;
 	private double price;
+	private String unity;
+	/**
+	 * @param name
+	 * @param stock
+	 * @param price
+	 */
+	public Product(String name, int stock, double price, String unity) {
+		super();
+		this.name = name;
+		this.stock = stock;
+		this.price = price;
+		this.unity = unity;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @return the stock
+	 */
+	public int getStock() {
+		return stock;
+	}
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
 	
+	/**
+	 * @return the unity
+	 */
+	public String getUnity() {
+		return unity;
+	}
 	
 }
