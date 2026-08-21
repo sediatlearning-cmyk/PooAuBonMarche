fd187006396c533aed9c7b629d8ab2a5f0929e28
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-08-21 13:32:49 +0200

    Added Cart class

--- src/fr/fms/entities/Cart.java
+++ src/fr/fms/entities/Cart.java
@@ -1,5 +1,41 @@
 package fr.fms.entities;
 
+import java.util.List;
+/*
+ * represents a new cart
+ */
 public class Cart {
 
+	private List<CartItem> items;
+	
+	/**
+	 * @param items
+	 */
+	public Cart(List<CartItem> items) {
+		super();
+		this.items = items;
+	}
+	
+	/**
+	 * @return the items
+	 */
+	public List<CartItem> getItems() {
+		return items;
+	}
+	
+	/**
+	 * Add a fruit in the list of cartItem with his quantity
+	 * @return the items
+	 */
+	public CartItem addFruit(Fruit fruit, float quantity) {
+		return null;
+	}
+	
+	/**
+	 * Add a fruit in the list of cartItem with his quantity
+	 * @return the items
+	 */
+	public CartItem addVegetable(Vegetable vegetable, float quantity) {
+		return null;
+	}
 }
