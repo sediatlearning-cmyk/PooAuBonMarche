9d9575068178abe64cffa67cd72e39a77ee1b69d
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-08-21 13:28:37 +0200

    Added Vegetable class that extends Product

--- src/fr/fms/entities/Vegetable.java
+++ src/fr/fms/entities/Vegetable.java
@@ -1,5 +1,22 @@
 package fr.fms.entities;
-
+/*
+ * represents a new vegetable
+ */
 public class Vegetable extends Product {
 
+	private boolean boolVegetable;
+	
+	public Vegetable(String name, int stock, double price, String unity, boolean booleanVegetable) {
+		super(name, stock, price, unity);
+		this.boolVegetable = booleanVegetable;
+	}
+
+	/**
+	 * @return the boolVegetable
+	 */
+	public boolean isBoolVegetable() {
+		return boolVegetable;
+	}
+
+	
 }
