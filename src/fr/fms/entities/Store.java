e6568448ea957600b05e53d556d3ff114c19bc2b
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-08-21 13:31:07 +0200

    Added Store class

--- src/fr/fms/entities/Store.java
+++ src/fr/fms/entities/Store.java
@@ -1,5 +1,62 @@
 package fr.fms.entities;
 
+import java.util.List;
+/*
+ * represents a new store
+ */
 public class Store {
 
+	private List<Fruit> fruits;
+	private List<Vegetable> vegetables;
+	private List<Customer> customers;
+	
+	/**
+	 * @param fruits
+	 * @param vegetables
+	 * @param customers
+	 */
+	public Store(List<Fruit> fruits, List<Vegetable> vegetables, List<Customer> customers) {
+		super();
+		this.fruits = fruits;
+		this.vegetables = vegetables;
+		this.customers = customers;
+	}
+	
+	/**
+	 * 
+	 */
+	public Store() {
+		super();
+	}
+
+
+
+	/**
+	 * @return the fruits
+	 */
+	public List<Fruit> getFruits() {
+		return fruits;
+	}
+
+	/**
+	 * @return the vegetables
+	 */
+	public List<Vegetable> getVegetables() {
+		return vegetables;
+	}
+
+	/**
+	 * @return the customers
+	 */
+	public List<Customer> getCustomers() {
+		return customers;
+	}
+
+	public void getMenu() {
+		System.out.println("-------------------------Menu-------------------------\n");
+		System.out.println("                  1- Nouveau client                   ");
+		System.out.println("                  2- Bilan                            ");
+		System.out.println("                  3- Fermer                           \n");
+		System.out.println("------------------------------------------------------");
+	}
 }
