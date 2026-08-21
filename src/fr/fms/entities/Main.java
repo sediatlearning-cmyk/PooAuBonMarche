f9c60f4dbdd394e7f38eb3ad7de73c749ffa5f8d
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-08-21 13:31:42 +0200

    Added Main class

--- src/fr/fms/entities/Main.java
+++ src/fr/fms/entities/Main.java
@@ -1,8 +1,35 @@
 package fr.fms.entities;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int choice = 0;
 		
+		Store store = new Store();
+		store.getMenu();
+		System.out.println("Quelle option choisissez vous? ");
+		choice = Integer.parseInt(scan.nextLine());
+		
+		switch (choice) {
+		
+		case 1:
+			
+			break;
+		case 2:
+			
+			break;
+			
+		case 3:
+			
+			break;
+		
+		default:
+			
+			 System.out.println("Choix invalide");
+		}
 	}
 }
