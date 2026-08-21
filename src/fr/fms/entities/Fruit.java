d9020f44d6d3be3534a5b3ae9de1533baa06e71a
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-08-21 13:29:17 +0200

    Added Fruit class that extends Product

--- src/fr/fms/entities/Fruit.java
+++ src/fr/fms/entities/Fruit.java
@@ -1,5 +1,22 @@
 package fr.fms.entities;
-
+/*
+ * represents a new fruit
+ */
 public class Fruit extends Product{
 
+	boolean boolFruit;
+	
+	public Fruit(String name, int stock, double price, String unity, boolean boolFruit) {
+		super(name, stock, price, unity);
+		this.boolFruit = boolFruit;
+	}
+
+	/**
+	 * @return the boolFruit
+	 */
+	public boolean isBoolFruit() {
+		return boolFruit;
+	}
+
+	
 }
