1c2d1e59adb99c4de3796b10d3a56dfb3fb0b013
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-08-21 13:32:22 +0200

    Added CartItem class

--- src/fr/fms/entities/CartItem.java
+++ src/fr/fms/entities/CartItem.java
@@ -1,5 +1,32 @@
 package fr.fms.entities;
-
+/*
+ * represents a line item in the customer's shopping cart
+ */
 public class CartItem {
+	
 
+	private Product product;
+	private float quantity;
+	/**
+	 * @param product
+	 * @param quantity
+	 */
+	public CartItem(Product product, float quantity) {
+		super();
+		this.product = product;
+		this.quantity = quantity;
+	}
+	/**
+	 * @return the product
+	 */
+	public Product getProduct() {
+		return product;
+	}
+	/**
+	 * @return the quantity
+	 */
+	public float getQuantity() {
+		return quantity;
+	}
+	
 }
